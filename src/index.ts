dde1c4b81443b7bf492031c13d2a3fde8a3e0056
JumpToSkyFree <alaoui.hamza031@gmail.com>
2024-07-30 19:48:21 +0100

    Fix exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@
 // export const ExampleComponent = ({ text }: Props) => {
 //   return <div className={styles.test}>Example Component: {text}</div>
 // }
-
-export * from './contexts/ServerContext'
-export * from './hooks/useConsumeServerContext'
-export * from './hooks/useServerContext'
-export * from './hooks/useRegisterServerContext'
+// export { default as useMyHook } from './hooks/useMyHook'
+export { default as ServerContext } from './contexts/ServerContext'
+export { default as useConsumeServerContext } from './hooks/useConsumeServerContext'
+export { default as useServerContext } from './hooks/useServerContext'
+export { default as useRegisterServerContext } from './hooks/useRegisterServerContext'
